aa21db408b9f2acd2bca7bf1d6c22fed2e362309
Bylack26 <fabilego3004@gmail.com>
2023-03-21 12:48:07 +0100

    Debut de projet

---
+++ client.c
@@ -0,0 +1,32 @@
+#include "csapp.h"
+
+int main(int argc, char **argv)
+{
+    int clientfd, port;
+    char *host, buf[MAXLINE];
+    rio_t rio;
+
+    if (argc != 3) {
+        fprintf(stderr, "usage: %s <host> <port>\n", argv[0]);
+        exit(0);
+    }
+    host = argv[1];
+    port = atoi(argv[2]);
+
+    clientfd = Open_clientfd(host, port);
+    
+    printf("client connected to server OS\n"); 
+    
+    Rio_readinitb(&rio, clientfd);
+
+    while (Fgets(buf, MAXLINE, stdin) != NULL) {
+        Rio_writen(clientfd, buf, strlen(buf));
+        if (Rio_readlineb(&rio, buf, MAXLINE) > 0) {
+            Fputs(buf, stdout);
+        } else { /* the server has prematurely closed the connection */
+            break;
+        }
+    }
+    Close(clientfd);
+    exit(0);
+}
